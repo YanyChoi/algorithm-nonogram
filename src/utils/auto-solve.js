313f0684a2e1cee847dc00ca8dafb650f5b18018
anonkorea4869 <anonkorea4869@anonkorea4869ui-MacBookAir.local>
2022-11-16 15:32:40 +0900

    check_conditional

--- src/utils/auto-solve.js
+++ src/utils/auto-solve.js
@@ -1,7 +1,36 @@
 const autoSolve = ({ row, column, table }) => {
-  console.log(row);
-  console.log(column);
-  console.log(table);
+  if(checkResult(row, column, table)) {
+    alert("SUCCESS");
+  } else {
+    alert("FAIL");
+  }
 };
+
+function checkResult(row, column, table) {
+  var SIZE = row.length;
+  var table_row = [];
+  var buffer = [];
+  var h = 0, w = 0;
+
+
+  // 가로줄 체크
+  for (w = 0 ; w < 1 ; w++) {
+    buffer = []; // 버퍼 초기화
+    for(h = 0 ; h < SIZE ; h++) {
+      if(table[h][w] == true) {
+        table_row[h].push(1);
+      } else {
+        table_row[h].push(0);
+      }
+    }
+  }
+
+  table_row.append()
+
+  console.log("table_row", table_row);
+
+  return 1;
+}
+
 
 export default autoSolve;
